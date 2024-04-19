96ae13d2f20de853cd5c0f84925b8881de1bd51e
sudeep ch <127744041+CHELLASUDEEP@users.noreply.github.com>
2024-04-19 11:49:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,160 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import QAGenerationChain
+from langchain.text_splitter import TokenTextSplitter
+from langchain.docstore.document import Document
+from langchain.document_loaders import PyPDFLoader
+from langchain.prompts import PromptTemplate
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.summarize import load_summarize_chain
+from langchain.chains import RetrievalQA
+import os 
+from PyPDF2 import PdfReader
+import csv
+
+os.environ["OPENAI_API_KEY"] = "YOUR_OPEN_API_KEY"
+
+# Set file path
+file_path = r'C:\Users\CH\Desktop\practice codes\chella\static\docs\Big Mac Index.pdf'
+
+def count_pdf_pages(pdf_path):
+    try:
+        pdf = PdfReader(pdf_path)
+        return len(pdf.pages)
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+def file_processing(file_path):
+
+    # Load data from PDF
+    loader = PyPDFLoader(file_path)
+    data = loader.load()
+
+    question_gen = ''
+
+    for page in data:
+        question_gen += page.page_content
+        
+    splitter_ques_gen = TokenTextSplitter(
+        model_name = 'gpt-3.5-turbo',
+        chunk_size = 10000,
+        chunk_overlap = 200
+    )
+
+    chunks_ques_gen = splitter_ques_gen.split_text(question_gen)
+
+    document_ques_gen = [Document(page_content=t) for t in chunks_ques_gen]
+
+    splitter_ans_gen = TokenTextSplitter(
+        model_name = 'gpt-3.5-turbo',
+        chunk_size = 1000,
+        chunk_overlap = 100
+    )
+
+    document_answer_gen = splitter_ans_gen.split_documents(document_ques_gen)
+
+    return document_ques_gen, document_answer_gen
+
+def llm_pipeline(file_path):
+
+    document_ques_gen, document_answer_gen = file_processing(file_path)
+
+    llm_ques_gen_pipeline = ChatOpenAI(
+        temperature = 0.3,
+        model = "gpt-3.5-turbo"
+    )
+
+    prompt_template = """
+    You are an expert at creating questions based on coding materials and documentation.
+    Your goal is to prepare a coder or programmer for their exam and coding tests.
+    You do this by asking questions about the text below:
+
+    ------------
+    {text}
+    ------------
+
+    Create questions that will prepare the coders or programmers for their tests.
+    Make sure not to lose any important information.
+
+    QUESTIONS:
+    """
+
+    PROMPT_QUESTIONS = PromptTemplate(template=prompt_template, input_variables=["text"])
+
+    refine_template = ("""
+    You are an expert at creating practice questions based on coding material and documentation.
+    Your goal is to help a coder or programmer prepare for a coding test.
+    We have received some practice questions to a certain extent: {existing_answer}.
+    We have the option to refine the existing questions or add new ones.
+    (only if necessary) with some more context below.
+    ------------
+    {text}
+    ------------
+
+    Given the new context, refine the original questions in English.
+    If the context is not helpful, please provide the original questions.
+    QUESTIONS:
+    """
+    )
+
+    REFINE_PROMPT_QUESTIONS = PromptTemplate(
+        input_variables=["existing_answer", "text"],
+        template=refine_template,
+    )
+
+    ques_gen_chain = load_summarize_chain(llm = llm_ques_gen_pipeline, 
+                                            chain_type = "refine", 
+                                            verbose = True, 
+                                            question_prompt=PROMPT_QUESTIONS, 
+                                            refine_prompt=REFINE_PROMPT_QUESTIONS)
+
+    ques = ques_gen_chain.run(document_ques_gen)
+
+    embeddings = OpenAIEmbeddings()
+
+    vector_store = FAISS.from_documents(document_answer_gen, embeddings)
+
+    llm_answer_gen = ChatOpenAI(temperature=0.1, model="gpt-3.5-turbo")
+
+    ques_list = ques.split("\n")
+    filtered_ques_list = [element for element in ques_list if element.endswith('?') or element.endswith('.')]
+
+    answer_generation_chain = RetrievalQA.from_chain_type(llm=llm_answer_gen, 
+                                                chain_type="stuff", 
+                                                retriever=vector_store.as_retriever())
+
+    return answer_generation_chain, filtered_ques_list
+
+def get_csv(file_path):
+    answer_generation_chain, ques_list = llm_pipeline(file_path)
+    base_folder = r'C:\Users\CH\Desktop\practice codes\chella\static\output'
+    if not os.path.isdir(base_folder):
+        os.mkdir(base_folder)
+    output_file = base_folder + f"{(file_path.split('/')[-1]).split('.')[0]}.csv"
+    with open(output_file, "w", newline='', encoding="utf-8") as csvfile:
+        fieldnames = ['Question', 'Answer']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for question in ques_list:
+            answer = answer_generation_chain.run(question)
+            writer.writerow({'Question': question, 'Answer': answer})
+    return output_file
+
+def main():
+    st.title("PDF Analyzer")
+
+    pdf_file = st.file_uploader("Upload PDF", type=['pdf'])
+    if pdf_file:
+        filename = pdf_file.name
+        st.write("Filename:", filename)
+        with open(filename, "wb") as f:
+            f.write(pdf_file.getbuffer())
+
+        st.write("Analyzing PDF...")
+        output_file = get_csv(filename)  
+        st.write("Analysis complete. Results saved to:", output_file)
+
+if __name__ == "__main__":
+    main()
